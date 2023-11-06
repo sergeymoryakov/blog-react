617e2225a16916f20d04998aa1ed9d3eb34b7da2
Sergey Moryakov <sergey@moryakov.com>
2023-11-06 17:00:39 +0200

    set loader indicator in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+const URL_USERS = "https://jsonplaceholder.typicode.com/users";
+
 function App() {
-    const [count, setCount] = useState(0);
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     console.log("Component rendered");
 
     useEffect(() => {
-        console.log("Call useEffect");
-        localStorage.setItem("count", count);
-    }, [count]);
+        setIsLoading(true);
 
-    // function handleButtonClick() {
-    //     localStorage.setItem("count", count);
-    //     setCount(count + 1);
-    // }
+        fetch(URL_USERS)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setUsers(data);
+                setIsLoading(false);
+            });
+        // .catch(error => console.log(error));
+    }, []);
 
     return (
         <div>
-            <div>{count}</div>
-            <button onClick={() => setCount(count + 1)}>Count++</button>
-            {/* <button onClick={handleButtonClick}>Count++</button> */}
+            {isLoading ? "Loading" : ""}
+            {JSON.stringify(users)}
         </div>
     );
 }
